d3eec7c6d21393f185775222568133934cf5cb4b
jonadar <jonadar2000@gmail.com>
2024-11-25 17:24:39 +0200

    functional add icecream form

--- src/components/NewIceCream.tsx
+++ src/components/NewIceCream.tsx
@@ -2,8 +2,9 @@ import { ChangeEvent, FunctionComponent, useState } from "react";
 
 import { HexColorString, IceCream } from "../consts/types";
 import IceCreamItem from "./IceCreamItem";
+import { useGoBack } from "../consts/hooks";
 
-import "../css/newIceCream.scss"
+import "../css/newIceCream.scss";
 
 interface NewIceCreamProps {
     onAdd: (newItem: IceCream) => void;
@@ -11,9 +12,11 @@ interface NewIceCreamProps {
 
 const NewIceCream: FunctionComponent<NewIceCreamProps> = ({ onAdd }) => {
 
+    const { goBack } = useGoBack();
+
     const [title, setTitle] = useState<string>('');
     const [description, setDescription] = useState<string>('');
-    const [color, setColor] = useState<HexColorString>('#ff00ff');
+    const [color, setColor] = useState<HexColorString>('#ffffff');
 
     const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setTitle(event.target.value);
@@ -27,29 +30,37 @@ const NewIceCream: FunctionComponent<NewIceCreamProps> = ({ onAdd }) => {
         setColor(event.target.value as HexColorString); //
     };
 
-    const handleAddIceCream = () => {
-        onAdd({ title, description, color })
+    const handleAddIceCream = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        onAdd({ title, description, color });
+        goBack();
     }
 
     return (
-        <div className="new-ice-cream-form">
+        <form className="new-ice-cream-form" method="none" onSubmit={handleAddIceCream}>
             <div>
-                <label htmlFor="title">title</label>
-                <input type="text" name="title" id="title" onChange={handleTitleChange} value={title} />
+                <button onClick={goBack}>back</button>
             </div>
             <div>
-                <label htmlFor="description">description</label>
-                <input type="text" name="description" id="description" onChange={handleDescriptionChange} value={description} />
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" id="title" onChange={handleTitleChange} value={title} placeholder="Title" maxLength={20} required />
             </div>
             <div>
-                <label htmlFor="color">color</label>
+                <label htmlFor="description">Description</label>
+                <input type="text" name="description" id="description" onChange={handleDescriptionChange} value={description} placeholder="Description" />
+            </div>
+            <div>
+                <label htmlFor="color">Color</label>
                 <div className="color-input-container">
                     <IceCreamItem item={{ title, description, color }} />
                     <input type="color" name="color" id="color" onChange={handleColorChange} value={color} />
                 </div>
             </div>
-            <button onClick={handleAddIceCream}>add</button>
-        </div>
+            <div>
+                <input type="submit" value="add"></input>
+            </div>
+        </form>
     );
 }
 
